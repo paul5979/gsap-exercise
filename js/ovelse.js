4860be417d5d71c9182de83383f65c33f2215dad
Pauline Hornbøll Nielsen <Pauline@paulines-macbook-air.home>
2020-10-12 10:51:11 +0200

    øvelser

--- js/ovelse.js
+++ js/ovelse.js
@@ -2,9 +2,35 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 
-gsap.to(".ball-1", {
+gsap.to(".ball-2", {
     x: 200,
     scale: .3,
-    duration: 2,
+    duration: 7,
     ease: "elastic.out(1, 0.3)",
+    scrollTrigger: {
+        trigger: ".ball-2",
+        markers: true,
+        start: "center 75%",
+        end: "bottom 25%",
+        toggleActions: "play pause resume reset"
+    }
 });
+
+
+gsap.from(".ball-3", {
+    x: 300,
+    scale: .3,
+    rotation: 360,
+    duration: 2,
+    ease: "bounce.out",
+    scrollTrigger: {
+        trigger: ".ball-3",
+        // markers: true,
+        start: "center center",
+        end: "center 25%",
+        scrub: true,
+
+
+    }
+
+})
